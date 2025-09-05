cee7ddd2488cef31413fac2defe73a4eddec1efb
daisuke <d.r.lzz1202@gmail.com>
2025-09-05 21:43:47 +0900

    mod: nav適当に調整

--- src/components/leftPanes/Navigation.vue
+++ src/components/leftPanes/Navigation.vue
@@ -1,5 +1,17 @@
 <template>
-  <v-btn color="primary">Vuetify ボタン</v-btn>
+  <v-container class="ma-0 pa-0 w-100 h-100">
+    <v-row no-gutters class="flex-column h-100">
+      <v-col cols="1" class="w-100"> </v-col>
+      <v-col cols="10" class="w-100">
+        <v-row no-gutters class="flex-column w-100 h-100 px-4">
+          <v-col cols="12" class="w-100">
+            <v-btn class="serch-btn w-100">検索</v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="1" class="w-100"> </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts">
@@ -8,4 +20,15 @@
 // defineProps<{ msg: string }>();
 </script>
 
-<style scoped></style>
+<style scoped>
+.v-col-1 {
+  max-width: none;
+}
+.v-col-10 {
+  max-width: none;
+}
+
+.serch-btn {
+  font-size: 24px;
+}
+</style>
