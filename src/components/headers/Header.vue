c4c340677e81aab57471aee124fc0bfde0c6bff6
daisuke <d.r.lzz1202@gmail.com>
2025-09-05 21:43:32 +0900

    mod: ヘッダー調整

--- src/components/headers/Header.vue
+++ src/components/headers/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container class="ma-0 pa-0 w-100 h-100">
-    <v-row no-gutters class="px-3 py-2 h-100 justify-space-between">
+    <v-row no-gutters class="px-6 py-2 h-100 justify-space-between">
       <v-col cols="3">
-        <div class="logo-wrapper d-flex align-center h-100">
-          <v-btn color="primary">Vuetify ボタン</v-btn>
+        <div class="title-wrapper d-flex align-center h-100 accent-font-color">
+          <h1>本検索システム</h1>
         </div>
       </v-col>
       <v-col cols="3">
